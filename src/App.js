7d67fd95d2f02cf3328d731aa6395d2e7fffe33d
lucianmot <lucian.i.mot@gmail.com>
2019-11-18 21:32:11 +0000

    attempt at pulling basic info from api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
-let url = "https://pro-api.coinmarket.com/v1/cryptocurrency/quotes/latest";
+let url = "https://pro-api.coinmarket.com/v1/cryptocurrency/quotes/latest",
+qString = "?CMC_PRO_API_KEY=" + "5370a625-752d-4be8-98be-187aa5d6c4a4" + "&start=1&limit=5&convert=USD";
+
+fetch(url + qString)
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
         <h1>
           Welcome to Crypto Currency Dashboard
         </h1>
